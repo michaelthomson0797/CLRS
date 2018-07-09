b802bdf301c564ade21988d97c8009dab318d26f
Michael Thomson <michaelthomson0797@gmail.com>
2018-07-09 22:47:45 +0000

    fixed heapsort in C to use structs

--- ch6/heapsort/C/heapsort.c
+++ ch6/heapsort/C/heapsort.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-void printArray(int *A, int n) {
-	for(int i = 0; i < n; i++) {
-		printf("%d, ", A[i]);
+struct heap {
+        int *A;
+        int length;
+        int heapSize;
+};
+
+void printArray(struct heap *h) {
+	for(int i = 0; i < h->length; i++) {
+		printf("%d, ", h->A[i]);
 	}
 	printf("\n");
 }
@@ -19,50 +26,55 @@ int right(int i) {
         return 2*i+2;
 }
 
-void swap (int *A, int i, int j) {
-        int tmp = A[i];
-        A[i] = A[j];
-        A[j] = tmp;
+void swap (struct heap *h, int i, int j) {
+        int tmp = h->A[i];
+        h->A[i] = h->A[j];
+        h->A[j] = tmp;
 }
         
 
-void maxHeapify(int *A, int i, int heapSize) {
+void maxHeapify(struct heap *h, int i) {
         int largest;
         int l = left(i);
         int r = right(i);
 
-        if(l < heapSize && A[l] > A[i]) {
+        if(l < h->heapSize && h->A[l] > h->A[i]) {
                 largest = l;
         } else largest = i;
 
-        if (r < heapSize && A[r] > A[largest]) {
+        if (r < h->heapSize && h->A[r] > h->A[largest]) {
                 largest = r;
         }
     
         if(largest != i) {
-                swap (A, i, largest);
-                maxHeapify(A, largest, heapSize);
+                swap (h, i, largest);
+                maxHeapify(h, largest);
         }
 }
 
-void buildMaxHeap(int *A, int heapSize) {
-        for(int i = heapSize/2-1; i >= 0; i--) {
-                maxHeapify(A, i, heapSize);
+void buildMaxHeap(struct heap *h) {
+        for(int i = h->heapSize/2-1; i >= 0; i--) {
+                maxHeapify(h, i);
         }
 }
 
-void heapsort(int *A, int heapSize) {
-        buildMaxHeap(A, heapSize);
-        for(int i = heapSize-1; i > 0; i--) {
-                swap(A, 0, i);
-                printArray(A, 10);
-                heapSize = heapSize - 1;
-                maxHeapify(A, 0, heapSize);
+void heapsort(struct heap *h) {
+        buildMaxHeap(h);
+        for(int i = h->heapSize-1; i > 0; i--) {
+                swap(h, 0, i);
+                h->heapSize--;
+                maxHeapify(h, 0);
         }
 }
 
 int main() {
         int A[] = {16,14,10,8,7,9,3,2,4,1};
-        heapsort(A, 10);
-        printArray(A, 10);
+
+        struct heap h;
+        h.heapSize = 10;
+        h.length = 10;
+        h.A = A;
+
+        heapsort(&h);
+        printArray(&h);
 }
